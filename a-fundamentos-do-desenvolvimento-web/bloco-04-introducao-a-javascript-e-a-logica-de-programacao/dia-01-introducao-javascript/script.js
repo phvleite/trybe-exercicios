26c702ad08c72faf17de631f364610c2c25bbc90
phvleite <phvleite@gmail.com>
2021-11-26 20:04:37 -0300

    inciando o javascript

---
+++ a-fundamentos-do-desenvolvimento-web/bloco-04-introducao-a-javascript-e-a-logica-de-programacao/dia-01-introducao-javascript/script.js
@@ -0,0 +1,28 @@
+// let myName = "Paulo Henrique Vieira Leite"
+// let birthCity = "Maceió"
+// let birthYear = 1969
+
+// console.log(myName);
+// console.log(birthCity);
+// console.log(birthYear);
+
+// birthYear = 2030
+
+// console.log(birthYear);
+
+// birthYear = birthYear + 15
+
+// console.log(birthYear);
+
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof(patientId));
+console.log(typeof(isEnrolled));
+console.log(typeof(patientInfo));
+console.log(typeof(patientEmail));
