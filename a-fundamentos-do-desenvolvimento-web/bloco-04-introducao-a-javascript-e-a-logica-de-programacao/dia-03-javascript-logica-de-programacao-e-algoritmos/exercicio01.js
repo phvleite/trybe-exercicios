cfd7e2dda8c67417ee5d8a379528ecca5c9e90d4
phvleite <phvleite@gmail.com>
2021-12-02 13:18:35 -0300

    exercicio 1 do dia 4.3

---
+++ a-fundamentos-do-desenvolvimento-web/bloco-04-introducao-a-javascript-e-a-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicio01.js
@@ -0,0 +1,8 @@
+let fatorial = 8;
+let resultadoFatorial = 1;
+
+for (let i = 0; i < fatorial; i += 1){
+    resultadoFatorial = resultadoFatorial * (fatorial - i)
+}
+
+console.log(resultadoFatorial);
