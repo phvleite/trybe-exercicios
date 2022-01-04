36cc2ccd0071c6de641cf6ca71eaa7f1a7509533
phvleite <phvleite@gmail.com>
2022-01-04 12:29:40 -0300

    fixando preventDefault

---
+++ a-fundamentos-do-desenvolvimento-web/bloco-06-html-e-css-forms-flexbox-e-responsivo/prevent-default/script.js
@@ -0,0 +1,18 @@
+// SELETORES
+const INPUT_TEXT = document.querySelector("#input-text");
+const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
+const HREF_LINK = document.querySelector("#href");
+
+HREF_LINK.addEventListener('click', (evento) => {
+    evento.preventDefault();
+})
+
+INPUT_CHECKBOX.addEventListener('click', (evento) => {
+    evento.preventDefault();
+})
+
+INPUT_TEXT.addEventListener('keypress', (evento) => {
+    if(evento.keyCode !== 65) {
+        evento.preventDefault();
+    }
+})
