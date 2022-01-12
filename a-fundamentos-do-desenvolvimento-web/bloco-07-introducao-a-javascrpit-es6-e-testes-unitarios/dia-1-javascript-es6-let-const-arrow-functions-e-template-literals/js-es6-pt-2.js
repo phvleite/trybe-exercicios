a377298fc8e4742c7ea95884297b4d085c449aa7
phvleite <phvleite@gmail.com>
2022-01-12 19:38:57 -0300

    ainda em solução

---
+++ a-fundamentos-do-desenvolvimento-web/bloco-07-introducao-a-javascrpit-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/js-es6-pt-2.js
@@ -0,0 +1,18 @@
+
+// function fatorial(n) {
+//     var resp = n;
+//     while (n > 2) {
+//         resp *= --n;
+//     }
+//     return resp;
+// }
+
+// // console.log(fatorial(5));
+
+// const n = 5;
+
+// const nFat = n < 1 ? 1 : fatorial(n);
+
+// console.log(nFat);
+
+const longestWord = ('Antônio foi no banheiro e não sabemos o que aconteceu');
