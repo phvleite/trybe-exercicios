dffadf6d9d8191127994a62efa6f1cafca7a363e
phvleite <phvleite@gmail.com>
2022-01-16 19:34:32 -0300

    exercício 7.1 questão 3

---
+++ a-fundamentos-do-desenvolvimento-web/bloco-07-introducao-a-javascrpit-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -0,0 +1,9 @@
+const btnClickCount = document.getElementById('btn-click-count');
+let count = 0;
+
+window.onload = () => {
+    btnClickCount.addEventListener('click', () => {
+        count += 1;
+        document.getElementById('score-click-count').innerHTML = count;
+    });
+}
