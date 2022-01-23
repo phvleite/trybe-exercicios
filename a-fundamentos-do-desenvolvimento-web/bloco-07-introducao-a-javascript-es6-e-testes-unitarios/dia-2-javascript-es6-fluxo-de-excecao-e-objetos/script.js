f470a834b2da02aadca3eb1aa2729dbd1b0e9b42
phvleite <phvleite@gmail.com>
2022-01-23 14:05:00 -0300

    finalizado 7.2 parte II

--- a-fundamentos-do-desenvolvimento-web/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/script.js
+++ a-fundamentos-do-desenvolvimento-web/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/script.js
@@ -15,14 +15,14 @@ const order = {
         pepperoni: {
           amount: 1,
           price: 20,
-        }
+        },
       },
       drinks: {
         coke: {
           type: 'Coca-Cola Zero',
           price: 10,
           amount: 1,
-        }
+        },
       },
       delivery: {
         deliveryPerson: 'Ana Silveira',
@@ -45,11 +45,36 @@ const order = {
     });
   }
 
-  customerInfo(order);
+  // customerInfo(order);
   
   const orderModifier = (order) => {
     // Adicione abaixo as informações necessárias.
-  
+    order.name = 'Luiz Silva';
+    order.payment.total = 50;
+    const consumption = [];
+    Object.keys(order.order).forEach((ord) => {
+      if (ord !== 'delivery') {
+        if (ord !== 'drinks') {
+          Object.keys(order.order[ord]).forEach((item) => {
+            consumption.push(item);        
+          });
+        } else {
+          Object.keys(order.order[ord]).forEach((item) => {
+            consumption.push(order.order[ord][item].type);
+          });
+        }
+      }
+    });
+    let finishedConsumption = '';
+    consumption.forEach((item, ind, arr) => {
+      if (ind < arr.length - 1) {
+        finishedConsumption += `${item}, `;
+      } else {
+        finishedConsumption += `e ${item} `;
+      }
+    });
+    const finished = `Olá ${order.name}, o total do seu pedido de ${finishedConsumption}é de R$${order.payment.total},00.`;
+    console.log(finished);
   }
   
-  //orderModifier(order);
+  orderModifier(order);
