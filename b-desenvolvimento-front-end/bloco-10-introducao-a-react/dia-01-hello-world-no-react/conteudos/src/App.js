42c41668ad16889848ebf1ca2c80daddbe891a3c
phvleite <phvleite@gmail.com>
2022-02-17 10:21:05 -0300

    exercicios em andamento

---
+++ b-desenvolvimento-front-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/conteudos/src/App.js
@@ -0,0 +1,15 @@
+import Header from './Header';
+// import Content from './Content';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <Header />
+      </header>
+    </div>
+  );
+}
+
+export default App;
